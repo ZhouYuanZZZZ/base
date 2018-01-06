8840a860acc75513325d598e95e2d44341cbfaab
zy <1270157082@qq.com>
2018-01-07 00:32:12 +0800

    list

---
+++ src/main/java/com/zy/algorithm/utils/app/StudyConllection.java
@@ -0,0 +1,33 @@
+package com.zy.algorithm.utils.app;
+
+import org.junit.Test;
+
+import java.util.*;
+
+public class StudyConllection {
+
+    @Test
+    public void test0(){
+        Integer[] a = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+
+        List<Integer> integers = Arrays.asList(a);
+        List<Integer> integers1 = integers.subList(0, 3);
+
+        System.out.println(integers.get(0) == integers1.get(0));//true
+    }
+
+    @Test
+    public void test1(){
+        TreeMap<String,String> map = new TreeMap<String, String>();
+
+        map.put("a","a");
+        map.put("b","b");
+        map.put("c","c");
+
+        SortedMap<String, String> stringStringSortedMap = map.subMap("a", "c");
+
+        System.out.println(stringStringSortedMap);
+
+
+    }
+}
