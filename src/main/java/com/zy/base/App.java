7bf41f3e67b3996a441307db910ae8d12d658160
zy <1270157082@qq.com>
2018-01-19 01:14:37 +0800

    base

---
+++ src/main/java/com/zy/base/App.java
@@ -0,0 +1,16 @@
+package com.zy.base;
+
+public class App {
+
+    public static void f1(double b){
+        System.out.println("double");
+    }
+
+    public static void f1(int i){
+        System.out.println("int");
+    }
+
+    public static void f1(short s){
+        System.out.println("short");
+    }
+}
---
+++ src/main/java/com/zy/base/Test0.java
@@ -0,0 +1,12 @@
+package com.zy.base;
+
+import org.junit.Test;
+
+public class Test0 {
+
+    @Test
+    public void test0(){
+        int s = 5;
+        App.f1(6);
+    }
+}
