b4b2e3212afa85e736c146ac23414bd9d14340bb
ZhouYuanZZZZ <1270157082@qq.com>
2018-04-25 18:38:58 +0800

    file

--- src/main/java/com/zy/rabbitMq/App.java
+++ src/main/java/com/zy/rabbitMq/App.java
@@ -1,4 +1,18 @@
 package com.zy.rabbitMq;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class App {
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        RPCClient rpcClient = new RPCClient();
+
+        long start = System.currentTimeMillis();
+        String result = rpcClient.call("xxxxdewfwrwer");
+        long end = System.currentTimeMillis();
+        System.out.println(result+":"+(end-start));
+
+        rpcClient.close();
+    }
 }
