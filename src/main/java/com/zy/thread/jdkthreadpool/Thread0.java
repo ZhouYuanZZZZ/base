997070ec572896d18127e11b4ae01c2ebd8090c5
ZhouYuanZZZZ <1270157082@qq.com>
2018-11-23 18:43:55 +0800

    thread

--- src/main/java/com/zy/thread/jdkthreadpool/Thread0.java
+++ src/main/java/com/zy/thread/jdkthreadpool/Thread0.java
@@ -3,6 +3,7 @@ package com.zy.thread.jdkthreadpool;
 import java.util.Date;
 
 public class Thread0 implements Runnable {
+
     public void run() {
         System.out.println(new Date()+":"+Thread.currentThread().getId());
         try {
